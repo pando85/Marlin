d96e8c773137b5d3a39fad20567f372c580cd8a6
pando85 <pando855@gmail.com>
2019-07-25 18:16:27 +0200

    Remove old K1 variable definition

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -471,8 +471,6 @@
   #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
                                   // is more then PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
   #define PID_INTEGRAL_DRIVE_MAX PID_MAX  //limit for the integral term
-  #define K1 0.95 //smoothing factor within the PID
-  #define PID_dT ((OVERSAMPLENR * 10.0)/(F_CPU / 64.0 / 256.0)) //sampling period of the temperature routine
 
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 // Ultimaker
