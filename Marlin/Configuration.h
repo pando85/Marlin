1c0b21fa64590f735ed91e3d35f409128b7fb22b
pando85 <pando855@gmail.com>
2019-07-26 18:47:09 +0200

    Fix steps per mm X and Y

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -714,7 +714,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2560, 730}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 40, 40, 2560, 730}  // default steps per unit for Ultimaker
 
 /**
  * Default Max Feed Rate (mm/s)
